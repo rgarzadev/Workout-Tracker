4dfed1d0728fcfb830e9f4f81b475721f26b2fab
Ryan Garza <ryangarza@live.com>
2020-11-10 23:14:48 -0600

    models

---
+++ models/workout.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            duration: {
+                type: Number,
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
